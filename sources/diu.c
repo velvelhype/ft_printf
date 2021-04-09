5335fc02cc2765e41c048a31ece66b485dd64ec2
velvelhype <velvelhype@gmail.com>
2021-04-10 06:10:32 +0900

    split printf.c

--- sources/diu.c
+++ sources/diu.c
@@ -3,18 +3,18 @@
 int diu(char *p, struct s_fls flag, va_list ap)
 {
 	char    *conved_v;
-    int     size_string;
+	int     size_string;
 
-    if (*p == 'u')
-    	conved_v = ft_itoa(va_arg(ap, unsigned int));
-    else
-    	conved_v = ft_itoa(va_arg(ap, int));
-    if (!conved_v)
-    	return (-1);
-    if (flag.prec < 0)
-    	flag.prec = -1;
-    size_string = dfinisher(conved_v, flag);
-    free(conved_v);
+	if (*p == 'u')
+		conved_v = ft_itoa(va_arg(ap, unsigned int));
+	else
+		conved_v = ft_itoa(va_arg(ap, int));
+	if (!conved_v)
+		return (-1);
+	if (flag.prec < 0)
+		flag.prec = -1;
+	size_string = dfinisher(conved_v, flag);
+	free(conved_v);
 
-    return (size_string);
+	return (size_string);
 }
