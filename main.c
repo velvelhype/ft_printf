6e0047a354db85c2d64c4d9b887fe987113ffb96
velvelhype <velvelhype@gmail.com>
2021-04-10 20:59:51 +0900

    dfin wordcount

--- main.c
+++ main.c
@@ -3,11 +3,23 @@
 
 int main()
 {
-	ft_printf("%% *.5i 42 == |% *.5i|\n", 4, 42); 
-	   printf("%% *.5i 42 == |%*.5i|\n", 4, 42);
-	   printf("%% *.5i 42 == |% *.5i|\n", 4, 42);
-	   printf("%% *.5i 42 == |%  *.5i|\n", 4, 42);
-	   printf("%% *.5i 42 == |%   *.5i|\n", 4, 42);
-	   printf("%% *.5i 42 == |%    *.5i|\n", 4, 42);
+	int p = 11;
+	int one = 1;
+	int zero = 0;
+	int minus = -1;
+	int mip = -11;
+	
+	if(!p)
+		printf("1\n");
+	if(!one)
+		printf("2\n");
+	if(!zero)
+		printf("3\n");
+	if(!minus)
+		printf("4\n");
+	if(!mip)
+		printf("5\n");
+
+	   system("leaks a.out");
 	
 }
